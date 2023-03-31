023c2e2ef4be6e243c68b4f5fa42186a441a1741
Danail Momchilov <46314846+Danail-Momchilov@users.noreply.github.com>
2023-03-31 16:29:26 +0300

    2023-03-31 - Completed Greenery object before troubleshooting

--- Greenery.cs
+++ Greenery.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace AreaCalculations
 {
@@ -43,14 +44,52 @@ namespace AreaCalculations
                         if (railing.LookupParameter("Railing Height").AsDouble() <= 6.56167979)
                             greenArea += (railing.LookupParameter("Length").AsDouble() * railing.LookupParameter("Railing Height").AsDouble());
                         else
-                            greenArea += (wall.LookupParameter("Length").AsDouble() * 6.56167979);
+                            greenArea += (railing.LookupParameter("Length").AsDouble() * 6.56167979);
                     }
                 }
             }
 
             else if (plotNames.Count == 2)
             {
+                foreach (Floor floor in allFloors)
+                {
+                    if (floor.LookupParameter("A Instance Area Plot").AsString() == plotNames[0])
+                        greenArea1 += floor.LookupParameter("Area").AsDouble();
+                    else
+                        greenArea2 += floor.LookupParameter("Area").AsDouble();
+                }
 
+                foreach (Wall wall in allWalls)
+                {
+                    double wallArea = 0;
+                    if (wall.WallType.LookupParameter("Green Area").AsInteger() == 1)
+                    {
+                        if (wall.LookupParameter("Unconnected Height").AsDouble() <= 6.56167979)
+                            wallArea += wall.LookupParameter("Area").AsDouble();
+                        else
+                            wallArea += (wall.LookupParameter("Length").AsDouble() * 6.56167979);
+                    }
+                    if (wall.LookupParameter("A Instance Area Plot").AsString() == plotNames[0])
+                        greenArea1 += wallArea;
+                    else
+                        greenArea2 += wallArea;
+                }
+
+                foreach (Railing railing in allRailings)
+                {
+                    double railingArea = 0;
+                    if (railing.LookupParameter("Green Area").AsInteger() == 1)
+                    {
+                        if (railing.LookupParameter("Railing Height").AsDouble() <= 6.56167979)
+                            railingArea += (railing.LookupParameter("Length").AsDouble() * railing.LookupParameter("Railing Height").AsDouble());
+                        else
+                            railingArea += (railing.LookupParameter("Length").AsDouble() * 6.56167979);
+                    }
+                    if (railing.LookupParameter("A Instance Area Plot").AsString() == plotNames[0])
+                        greenArea1 += railingArea;
+                    else
+                        greenArea2 += railingArea;
+                }
             }
         }
     }
