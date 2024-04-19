5bc79130e549449dfa448a79ac4ef3b34c024d01
Danail Momchilov <46314846+Danail-Momchilov@users.noreply.github.com>
2024-04-19 18:57:12 +0300

    2024-04-19
    New Window Added for set Area Coefficients Method

---
+++ AreaCoefficientsWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows;
+
+namespace AreaCalculations
+{
+    /// <summary>
+    /// Interaction logic for AreaCoefficientsWindow.xaml
+    /// </summary>
+    public partial class AreaCoefficientsWindow : Window
+    {
+        public AreaCoefficientsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetAreaCoefficients(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
